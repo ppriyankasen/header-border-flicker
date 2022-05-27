3ff205d75372e7e7ffc4568761f460afb363ad2b
Channamsetty, Vijay Vamsi <vchannamsetty@bcbsm.com>
2022-05-27 12:51:09 -0400

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { IonRouterOutlet, ModalController } from '@ionic/angular';
+import { LearnComponent } from '../learn/learn.component';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private modalController: ModalController,
+    private routerOutlet: IonRouterOutlet,
+    private elementRef: ElementRef,
+  ) {}
+
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: LearnComponent,
+      cssClass: 'my-custom-class',
+      mode: 'ios',
+      swipeToClose: true,
+      presentingElement: this.routerOutlet.nativeEl
+    });
+    void modal.present();
+  }
 
 }
